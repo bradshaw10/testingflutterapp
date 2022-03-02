d0e0b38321b347a43f684fa7bca4f01a785fc246
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-03-02 21:19:22 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,10 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -17,7 +21,7 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             Text('Question'),
-            RaisedButton(child: Text('Answer 1'), onPressed: null),
+            RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
             RaisedButton(child: Text('Answer 2'), onPressed: null),
             RaisedButton(child: Text('Answer 3'), onPressed: null),
           ],
