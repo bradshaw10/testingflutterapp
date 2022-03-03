22ee32b4b4475378c2413c83f3c830aeeea7570d
bradshaw10 <eoghan.bradshaw@gmail.com>
2022-03-03 12:56:52 +0400

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,8 +22,8 @@ class MyApp extends StatelessWidget {
           children: [
             Text('Question'),
             RaisedButton(child: Text('Answer 1'), onPressed: answerQuestion),
-            RaisedButton(child: Text('Answer 2'), onPressed: null),
-            RaisedButton(child: Text('Answer 3'), onPressed: null),
+            RaisedButton(child: Text('Answer 2'), onPressed: answerQuestion),
+            RaisedButton(child: Text('Answer 3'), onPressed: answerQuestion),
           ],
         ),
       ),
